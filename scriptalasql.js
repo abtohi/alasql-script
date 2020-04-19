19cfbfca1872cb568902103887b13e577d18d991
Slamet Abtohi <slametabtohi18@gmail.com>
2020-04-19 21:00:32 +0700

    Update scriptalasql.js

--- scriptalasql.js
+++ scriptalasql.js
@@ -1,14 +1,13 @@
 function test(){
 eval(UrlFetchApp.fetch('https://raw.githubusercontent.com/abtohi/alasql/master/alasql.js').getContentText());
-let data = [['a','b'],[2,20],[2,30],[4,50]]
-let headers = data.shift()
+let headers  = data.shift()
 let newArray = data.map(r => {
-                         let obj = {};
+               let obj = {};
            
            r.forEach((cell, i) => {
     obj[headers[i]] = cell;
   })
     
-    return obj;
+  return obj;
            })
 }
